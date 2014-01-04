77e721ef2fd22bf9538263a598da2c2e4e5921be
Yan Wang <grapeot@gmail.com>
2014-01-04 07:11:19 -0500

    rename the crawler

--- amazon.sh
+++ amazon.sh
@@ -1,1 +1,1 @@
-echo "$(date) $(phantomjs /home/grapeot/Dropbox/tmp/AmazonPrice/amazon.js)" >> /home/grapeot/Dropbox/tmp/AmazonPrice/price.txt
+echo "$(date) $(phantomjs /home/grapeot/Dropbox/tmp/AmazonPrice/crawl.js)" >> /home/grapeot/Dropbox/tmp/AmazonPrice/price.txt
